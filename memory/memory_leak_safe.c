c315fe0ca833a9dfe721e29c59ec5920c5db216c
TheMatt2 <TheMatt2@users.noreply.github.com>
2023-09-11 21:06:46 -0400

    Add Windows / Linux Support to memory_leak_safe.c
    Windows and Linux define alloc in a different header than MacOS (BSD).
    
    Add OS checks to accomodate.

--- memory/memory_leak_safe.c
+++ memory/memory_leak_safe.c
@@ -5,12 +5,21 @@
  * While this program will not crash, leaking memory consumes resources
  * that the OS is not guaranteed to reclaim in a timely manner.
  *
- * The exception is memory allocated by alloc, which does not need to
+ * The exception is memory allocated by alloca, which does not need to
  * be freed.
  */
 
 #include <stdio.h>
-#include <stdlib.h>
+
+#if defined(_MSC_VER)
+// On Windows, _alloca() is in malloc.h
+#include <malloc.h>
+#define alloca _alloca
+#else
+// On Linux alloca() is defined in alloca.h
+// On MacOS alloca() is defined in alloca.h and stdlib.h
+#include <alloca.h>
+#endif
 
 int main(int argc, char *argv[]) {
     (void) argc;
